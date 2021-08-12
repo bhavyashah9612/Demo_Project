08981903374958700ad500444e96fe632e52788d
Bhavya Shah <https://github.com/bhavyashah9612>
2021-08-11 23:02:56 -0400

    First commit

---
+++ src/main/java/com/example/cartoonhero/CartoonHeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartoonhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoonHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoonHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cartoonhero/CartoonHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartoonhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoonHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
